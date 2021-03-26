87d4681a223d04f66ddf4ee05aa90eb1644c6ec1
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2021-03-26 12:57:29 -0500

    including tools

---
+++ exercises/exercises-mpi-c/tools.h
@@ -0,0 +1,22 @@
+/****************************************************************
+ ****
+ **** This program file is part of the book 
+ **** `Parallel programming with MPI and OpenMP'
+ **** by Victor Eijkhout, eijkhout@tacc.utexas.edu
+ ****
+ **** copyright Victor Eijkhout 2012-2021
+ ****
+ **** tools.h : support routine prototypes
+ ****
+ ****************************************************************/
+
+#include <math.h>
+
+#define ISAPPROX(x,y) \
+  ( ( x==0. && fabs(y)<1.e-14 ) || ( y==0. && fabs(x)<1.e-14 ) || \
+    fabs(x-y)/fabs(x)<1.e-14 )
+
+double array_error(double ref_array[],double value_array[],int array_size);
+int test_all_the_same_int( int value,MPI_Comm comm );
+void print_final_result( int cond,MPI_Comm comm );
+
